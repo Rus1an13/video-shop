f9d6388cf258779dee22fc2116b63d4078fe7277
Rus1an13 <ruslan-1978@bk.ru>
2025-09-20 20:17:46 +0500

    Магазин курсов

--- videostore/videostore/settings.py
+++ videostore/videostore/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'courses',
+    'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +119,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "pictures")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'user'
+LOGIN_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
